b77fec7f70f634cc73f39d3d90507952f4e21930
makis <makis@MAXIMSOMMER>
2022-01-13 15:05:59 +0100

    Add project files.

---
+++ GuessingGame/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace GuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tmrLabelHide = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrLabelHide
+            // 
+            this.tmrLabelHide.Enabled = true;
+            this.tmrLabelHide.Interval = 2000;
+            this.tmrLabelHide.Tick += new System.EventHandler(this.tmrLabelHide_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(882, 628);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrLabelHide;
+    }
+}
+
---
+++ GuessingGame/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BuildGrid();
+        }
+
+        private const int BLOCK_WIDTH = 100;
+        private const int BLOCK_HEIGHT = 100;
+        private List<Label> labels = new List<Label>();
+        private Random random = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void BuildGrid()
+        {
+            int labelIndex = 1;
+            for(int i = 0; i < this.Height; i += BLOCK_HEIGHT)
+            {
+                for (int j = 0; j < this.Width; j += BLOCK_WIDTH)
+                {
+                    Label lbl = new Label();
+                    lbl.Location = new Point(j, i);
+                    lbl.AutoSize = false;
+                    lbl.Size = new Size(BLOCK_WIDTH, BLOCK_HEIGHT);
+                    lbl.Text = labelIndex.ToString();
+                    lbl.TextAlign = ContentAlignment.MiddleCenter;
+                    lbl.Click += lbl_Click;
+                    labelIndex++;
+                    this.Controls.Add(lbl);
+                    this.labels.Add(lbl);
+                }
+            }
+        }
+
+        private void lbl_Click(object sender, EventArgs e)
+        {
+            if ((sender is Label) == false)
+            {
+                throw new ArgumentException("Unexpected type of sender");
+            }
+            Label lbl = (Label)sender;
+            lbl.Visible = false;
+        }
+
+        private void tmrLabelHide_Tick(object sender, EventArgs e)
+        {
+            int index = random.Next(0, this.labels.Count);
+            Label lbl = this.labels[index];
+            lbl.Visible = false;
+            this.labels.RemoveAt(index);
+            //this.labels.Remove(lbl);
+            if (this.labels.Count == 0) this.tmrLabelHide.Enabled = false;
+        }
+    }
+}
